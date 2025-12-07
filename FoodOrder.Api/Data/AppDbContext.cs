3cb79c95ee2a84db545d5e20cf43aeeb56215d08
Angel <a4k0o@protonmail.com>
2025-12-07 04:27:51 +0200

    Init
    Basic project structure and some models

---
+++ FoodOrder.Api/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using FoodOrder.Api.Models;
+
+namespace FoodOrder.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
